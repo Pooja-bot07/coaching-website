90bcfec620a08a03e98d6ced8cf6779eb5bb4b87
Pooja Malusare <poojamalusare22@gmail.com>
2026-08-21 10:52:36 +0530

    Initial coaching website

---
+++ js/script.js
@@ -0,0 +1,18 @@
+<a
+    href="https://www.instagram.com/saraswati.classes26/"
+    target="_blank"
+    rel="noopener noreferrer"
+    class="contact-detail contact-link"
+>
+
+    <div class="contact-icon">
+        <i class="fa-brands fa-instagram"></i>
+    </div>
+
+    <div>
+        <span>INSTAGRAM</span>
+        <strong>saraswati.classes26</strong>
+        <small>Follow us for updates</small>
+    </div>
+
+</a>
